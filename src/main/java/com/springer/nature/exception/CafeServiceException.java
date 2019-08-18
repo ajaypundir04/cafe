079f347b7fc6a460c0e938b2960f3f48657b5951
Ajay Singh Pundir <ajaysinghpundir70@gmail.com>
2019-08-18 22:39:15 +0530

    Removal Unnecessary code

--- src/main/java/com/springer/nature/exception/CafeServiceException.java
+++ src/main/java/com/springer/nature/exception/CafeServiceException.java
@@ -4,12 +4,4 @@ public class CafeServiceException extends RuntimeException {
     public CafeServiceException(String message) {
         super(message);
     }
-
-    public CafeServiceException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public CafeServiceException(Throwable cause) {
-        super(cause);
-    }
 }
--- src/main/java/com/springer/nature/exception/MenuServiceException.java
+++ src/main/java/com/springer/nature/exception/MenuServiceException.java
@@ -4,12 +4,4 @@ public class MenuServiceException extends RuntimeException {
     public MenuServiceException(String message) {
         super(message);
     }
-
-    public MenuServiceException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public MenuServiceException(Throwable cause) {
-        super(cause);
-    }
 }
